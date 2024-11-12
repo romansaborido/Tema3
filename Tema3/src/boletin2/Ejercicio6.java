ae394484d3103cf224700b13149c82b1e9f618af
romansaborido <roman.saborido@iesnervion.es>
2024-11-12 14:34:15 +0100

    Boletin2 terminado

--- Tema3/src/boletin2/Ejercicio6.java
+++ Tema3/src/boletin2/Ejercicio6.java
@@ -16,22 +16,44 @@ public class Ejercicio6 {
 		// Creamos la variable numeroAciertos para almacenar el numero de aciertos
 		int numeroAciertos = 0;
 		
-		// Creamos la variable posicion para almacenar la posicion de un valor a buscar
-		int posicion;
+		// Creamos la variable posicionUsuario para almacenar la posicion de un valor a buscar en la tabla cuponUsuario
+		int posicionUsuario = 0;
+		
+		// Creamos la variable posicionGanador para almacenar la posicion de un valor a buscar en la tabla cuponGanador
+		int posicionGanador = 0;
+		
+		// Creamos la variable busqueda y busqueda2 para almacenar los resultados de las busquedas
+		int busqueda;
+		int busqueda2;
+		
+		// Creamos la variable numeroAleatorio para ir almacenando los numeros aleatorios que vamos generando
+		int numeroAleatorio;
 		
 		// Creamos la clase random para generar numeros aleatorios
 		Random rand = new Random();
 		
 		// Asignamos los valores del array cuponUsuario
-		for (int i = 0 ; i < cuponUsuario.length ; i++) {
-			cuponUsuario[i] = rand.nextInt(1,50);
+		while (posicionUsuario < cuponUsuario.length) {
+			numeroAleatorio = rand.nextInt(1,50);
+			Arrays.sort(cuponUsuario);
+			busqueda = Arrays.binarySearch(cuponUsuario, numeroAleatorio);
+			if (busqueda < 0) {
+				cuponUsuario[0] = numeroAleatorio;
+				posicionUsuario++;
+			}
 		}
 		
 		// Asignamos los valores del array cuponGanador
-		for (int i = 0 ; i < cuponGanador.length ; i++) {
-			cuponGanador[i] = rand.nextInt(1,50);
+		while (posicionGanador < cuponGanador.length) {
+			numeroAleatorio = rand.nextInt(1,50);
+			Arrays.sort(cuponGanador);
+			busqueda = Arrays.binarySearch(cuponUsuario, numeroAleatorio);
+			if (busqueda < 0) {
+				cuponUsuario[0] = numeroAleatorio;
+				posicionGanador++;
+			}
 		}
-		
+			
 		// Ordenamos los valores del array cuponGanador
 		Arrays.sort(cuponGanador);
 		
@@ -51,9 +73,9 @@ public class Ejercicio6 {
 		
 		// Buscamos los valores del array cuponUsuario en el array ordenado cuponGanador
 		for (int i = 0 ; i < cuponUsuario.length ; i++) {
-			posicion = Arrays.binarySearch(cuponGanador, cuponUsuario[i]);
+			busqueda2 = Arrays.binarySearch(cuponGanador, cuponUsuario[i]);
 			
-			if (posicion >= 0) {
+			if (busqueda2 >= 0) {
 				numeroAciertos++;
 			}	
 		}
--- Tema3/src/boletin2/Ejercicio7.java
+++ Tema3/src/boletin2/Ejercicio7.java
@@ -6,17 +6,18 @@ public class Ejercicio7 {
 
 	public static void main(String[] args) {
 		
-		// Creamos el array secuenciaValores de longitud 55
-		int secuenciaValores[] = new int[1];
+		// Creamos el array secuenciaValores e inicializamos el tamaño a la variable tamaño
+		int secuenciaValores[] = new int[55];
 		
-		// Asignamos los valores del array
+		// Creamos la variable inicio para ir controlando el inicio del rango del fill
+		int inicio = 0;
+		
+		// Creamos el for para ir generando el valor a rellenar
 		for (int i = 1 ; i <= 10 ; i++) {
-			System.out.print(i);
-			for (int j = 0 ; j < i - 1 ; j++) {
-				System.out.print(i);
-				Arrays.fill(secuenciaValores, j);
-			}
+			Arrays.fill(secuenciaValores, inicio, inicio + i, i);
 			
+			// Incrementamos inicio con el valor a rellenar
+			inicio += i;
 		}
 		
 		// Mostramos el array
