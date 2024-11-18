08c7b60538eacbef849505adab5bda996bf8b675
romansaborido <roman.saborido@iesnervion.es>
2024-11-18 18:11:45 +0100

    Ejercicio mosca terminado

--- Tema3/src/englishexercises/Exercise3.java
+++ Tema3/src/englishexercises/Exercise3.java
@@ -8,73 +8,102 @@ import java.util.Scanner;
 public class Exercise3 {
 
 	public static void main(String[] args) {
-		
-		// Creamos el array de longitud 10
+
+		// Create the array of length 10
 		int array[] = new int[10];
-		
-		// Creamos la clase random para generar la posicion de la mosca aleatoriamente
+
+		// Create the random class to generate the random position of the fly
 		Random rand = new Random();
-		
-		// Creamos la variable posicionMosca para ir almacenando la posicion de la mosca
-		int posicionMosca = rand.nextInt(0,10);
-		
-		// Creamos la variable posicionUsuario para ir almacenando la posicion que introduzca el usuario
-		int posicionUsuario = 0;
-		
-		// Creamos el scanner
+
+		// Create the variable flyPosition to store the position of the fly
+		int flyPosition = rand.nextInt(0, 10);
+
+		// Create the variable userPosition to store the position entered by the user
+		int userPosition = 0;
+
+		// Create the scanner
 		Scanner reader = new Scanner(System.in);
+
+		// Assign the fly (1) in the random position
+		array[flyPosition] = 1;
+
+		// Ask the user in what position he thinks the fly is 
+		do {
+			try {
+				System.out.println("Enter the position you think the fly is");
+				userPosition = reader.nextInt();
+				assert (userPosition >= 0 && userPosition <= 9) : "Enter a valid position (0 to 9)";
+			} catch (AssertionError e) {
+				System.err.println(e.getMessage());
+			} catch (InputMismatchException e) {
+				System.err.println("Enter an integer number");
+				reader.next();
+			}
+		} while (userPosition < 0 || userPosition > array.length - 1);
+
 		
-		// Asignamos la mosca (1) en la posicion aleatoria
-		array[posicionMosca] = 1;
-		
-		// Creamos el bucle para preguntar al usuario
-		
-			do {
+		// As long as the value of array[userPosition] is not equal to 1
+		while (array[userPosition] != 1) {
+			
+			// If the userPosition is 0
+			if (userPosition == 0) {
 				
+				// Check that the fly is in the next position
+				if (array[userPosition + 1] == 1) {
+					System.out.println("You have been very close. You have scared the fly and has changed of position");
+					Arrays.fill(array, 0);
+					flyPosition = rand.nextInt(0, 10);
+					array[flyPosition] = 1;
+				} else {
+					System.out.println("The fly is far. The flay is en the same position");
+				}
+				
+			// If the userPosition is 9
+			} else if (userPosition == 9) {
+				
+				// Check that the fly is in the before position
+				if (array[userPosition - 1] == 1) {
+					System.out.println("You have been very close. You have scared the fly and has changed of position");
+					Arrays.fill(array, 0);
+					flyPosition = rand.nextInt(0, 10);
+					array[flyPosition] = 1;
+				} else {
+					System.out.println("The fly is far. The flay is en the same position");
+				}
+				
+			// If the userPosition is not 0 or 9. Check that the fly is in the before position or next position
+			} else if (array[userPosition - 1] == 1 || array[userPosition + 1] == 1) {
+				System.out.println("You have been very close. You have scared the fly and has changed of position");
+				Arrays.fill(array, 0);
+				flyPosition = rand.nextInt(0, 10);
+				array[flyPosition] = 1;
+			} else {
+				System.out.println("The fly is far. The flay is en the same position");
+			}
+			
+			// Ask the user in what position he thinks the fly is 
+			do {
 				try {
-					System.out.println("Introduzca la posicion en la que tu crees que esta la mosca");
-					posicionUsuario = reader.nextInt();
-					assert (posicionUsuario >= 0 && posicionUsuario <= 9) : "Introduzca una posicion valida (0 a 9)";
+					System.out.println("Enter the position you think the fly is");
+					userPosition = reader.nextInt();
+					assert (userPosition >= 0 && userPosition <= 9) : "Enter a valid position (0 to 9)";
 				} catch (AssertionError e) {
 					System.err.println(e.getMessage());
 				} catch (InputMismatchException e) {
-					System.err.println("Introduzca un número entero");
+					System.err.println("Enter an integer number");
+					reader.next();
 				}
-				
-				if (posicionUsuario == 0) {
-					if (array[posicionUsuario + 1] == 1) {
-						System.out.println("Has estado muy cerca. Has espantado a la mosca y ha cambiado de posicion");
-						Arrays.fill(array, 0);
-						posicionMosca = rand.nextInt(0,10);
-						array[posicionMosca] = 1;
-					} else {
-						System.out.println("La mosca está lejos. No se ha movido de posicion");
-					}
-				} else if (posicionUsuario == 9) {
-					if (array[posicionUsuario - 1] == 1) {
-						System.out.println("Has estado muy cerca. Has espantado a la mosca y ha cambiado de posicion");
-						Arrays.fill(array, 0);
-						posicionMosca = rand.nextInt(0,10);
-						array[posicionMosca] = 1;
-					} else {
-						System.out.println("La mosca está lejos. No se ha movido de posicion");
-					}
-				} else if (array[posicionUsuario - 1] == 1 || array[posicionUsuario + 1] == 1) {
-					System.out.println("Has estado muy cerca. Has espantado a la mosca y ha cambiado de posicion");
-					Arrays.fill(array, 0);
-					posicionMosca = rand.nextInt(0,10);
-					array[posicionMosca] = 1;
-				} else {
-					System.out.println("Has atrapado a la mosca");
-				}
-				
-			} while (array[posicionUsuario] != 1);
+			} while (userPosition < 0 || userPosition > array.length - 1);
 			
-			// Cerramos el scanner
-			reader.close();
+		}
 		
+		// You have caught the fly
+		System.out.println("You have caught the fly");
 		
+		// Close the scanner
+		reader.close();
 
+		
 	}
 
 }
