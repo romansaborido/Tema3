6a80c36510bb816c9c608f7d6d27e5f82e426681
HP <HP@DESKTOP-I865B9H>
2024-11-19 17:55:32 +0100

    Ejercicio 4 terminado

--- Tema3/src/boletin4/Ejercicio3.java
+++ Tema3/src/boletin4/Ejercicio3.java
@@ -7,55 +7,39 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 
+	// Creamos el scanner
+	static Scanner reader = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 
-		// Creamos el scanner
-		Scanner reader = new Scanner(System.in);
-
+		// Creamos el array tablaPares para almacenar el resultado de la funcion
+		int tablaPares[];
+		
 		// Creamos la variable longitudArray para almacenar la longitud de la tabla que introduzca el usuario
 		int longitudArray = 0;
 
 		// Creamos la variable valorFinal para almacenar el valor maximo del rango
-		int valorFinal = 0;
-
-		// Creamos el array tablaPares para almacenar el resultado de la funcion
-		int tablaPares[] = new int[longitudArray];
+		int valorFinal;
 		
 		// Pedimos al usuario que introduzca la longitud del array
-		do {
-			try {
-				System.out.println("Introduzca la longitud de la tabla");
-				longitudArray = reader.nextInt();
-				assert (longitudArray > 0) : "Introduzca un número mayor que 0";
-			} catch (AssertionError e) {
-				System.err.println(e.getMessage());
-			} catch (InputMismatchException e) {
-				System.err.println("Introduzca un número entero");
-			} finally {
-				reader.nextLine();
-			}
-		} while (longitudArray <= 0);
-
-		// Pedimos al usuario que introduzca la longitud del array
-		do {
-			try {
-				System.out.println("Introduzca el valor final de la tabla");
-				valorFinal = reader.nextInt();
-				assert (valorFinal > 2) : "Introduzca un número mayor que 2";
-			} catch (AssertionError e) {
-				System.err.println(e.getMessage());
-			} catch (InputMismatchException e) {
-				System.err.println("Introduzca un número entero");
-			} finally {
-				reader.nextLine();
-			}
-		} while (valorFinal < 2);
-
+		longitudArray = pedirNumeroEntero("Introduzca la longitud del array");
+		
+		// Pedimos al usuario que introduzca el valor maximo del rango
+		valorFinal = pedirNumeroEntero("Introduzca el valor maximo del array");
+		
 		// Almacenamos el resultado de la funcion
-		System.out.println(Arrays.toString(rellenaPares(longitudArray, valorFinal)));
+		tablaPares = rellenaPares(longitudArray, valorFinal);
 		
+		// Salto de linea
+		System.out.println();
+		
+		// Imprimimos el array
+		System.out.println(Arrays.toString(tablaPares));
+		
+		// Cerramos el scanner
+		reader.close();
 	}
-	
+
 	static int[] rellenaPares(int longitud, int fin) {
 
 		// Creamos la tabla a devolver de la longitud que el usuario indique
@@ -63,19 +47,56 @@ public class Ejercicio3 {
 
 		// Creamos la clase random para generar numeros aleatorios
 		Random rand = new Random();
-		
+
 		// Creamos la variable contador para almacenar el numero de veces que almacenamos un valor par en el arary
 		int contador = 0;
 		
+		// Creamos la variable numeroRandom para almacenar el numero aleatorio que generemos
+		int numeroRandom;
+		
+		// Mientras que el contador sea menor que la longitud de la tabla
 		while (contador < tabla.length) {
 			
+			// Generamos un numero random
+			numeroRandom = rand.nextInt(2, fin + 1);
+			
+			// Si el numero random es par lo almacenamos en el array e incrementamos contador
+			if (numeroRandom % 2 == 0) {
+				tabla[contador] = numeroRandom;
+				contador++;
+			}
 		}
-		
 
 		// Ordenamos la tabla
 		Arrays.sort(tabla);
-
+		
+		// Devolvemos la tabla
 		return tabla;
+	}
+
+	static int pedirNumeroEntero(String mensaje) {
+		
+		// Creamos la variable a devolver numero y la incializamos a 0
+		int numero = 0;
+		
+		do {
+			try {
+				System.out.println(mensaje);
+				numero = reader.nextInt();
+				assert (numero > 0) : "Introduzca un número mayor que 0";
+			} catch (AssertionError e) {
+				System.err.println(e.getMessage());
+			} catch (InputMismatchException e) {
+				System.err.println("Introduzca un número entero");
+			} finally {
+				reader.nextLine();
+			}
+		} while (numero <= 0);
+		
+		// Devolvemos el numero
+		return numero;
 	}
 	
 }
+
+
---
+++ Tema3/src/boletin4/Ejercicio4.java
@@ -0,0 +1,71 @@
+package boletin4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		// Creamos el scanner 
+		Scanner reader = new Scanner(System.in);
+		
+		// Creamos la tabla
+		int tabla[] = {0, 5, 3, 10, 21, 43, 6, 8, 18};
+		
+		// Creamos la variable valorBuscar para almacenar el valor a buscar
+		int valorBuscar;
+		
+		// Creamos la variable busqueda para almacenar el resultado de la funcion
+		int busqueda;
+		
+		// Pedimos al usuario que introduzca un valor a buscar 
+		System.out.println("Introduzca un valor a buscar. Te dire en que posicion se encuentra. Si no esta en la tabla te devolvere -1");
+		valorBuscar = reader.nextInt();	
+		
+		// Almacenamos el resultado de la funcion 
+		busqueda = buscar(tabla, valorBuscar);
+		
+		// Salto de linea
+		System.out.println();
+		
+		// Imprimimos el resultado de la busqueda
+		System.out.println("Resultado de la busqueda -> " + busqueda);
+		
+		// Salto de linea
+		System.out.println();
+		
+		// Imprimimos la tabla para que el usuario compruebe el resultado
+		System.out.println("Si deseas puedes comprobar el resultado");
+		System.out.println(Arrays.toString(tabla));
+		
+		// Cerramos el scanner
+		reader.close();
+		
+	}
+	
+	static int buscar(int t[], int clave) {
+		
+		// Creamos la variable busqueda para almacenar el resultado de esta
+		int busqueda = 0;
+		
+		// Creamos la variable posicion para ir almacenando la posicion de la busqueda secuencial
+		int posicion = 0;
+		
+		// Mientras que la posicion sea menor que la longitud de la tabla y la clave no sea igual que el valor
+		while (posicion < t.length && clave != t[posicion]) {
+			posicion++;
+		}
+		
+		// Si posicion es menor que la longitud de la tabla, busqueda vale posicion, sino -1
+		if (posicion < t.length) {
+			busqueda = posicion;
+		} else {
+			busqueda = -1;
+		}
+		
+		// Devolvemos el resultado de la busqueda
+		return busqueda;
+	}
+
+}
