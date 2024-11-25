5b236e49738a3cdb8987fdaefaf50dd34c24281c
romansaborido <roman.saborido@iesnervion.es>
2024-11-25 11:16:05 +0100

    Hacer ejercicios juegos

--- Tema3/src/boletin5/Ejercicio1.java
+++ Tema3/src/boletin5/Ejercicio1.java
@@ -1,6 +1,8 @@
 package boletin5;
 
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
@@ -9,6 +11,9 @@ public class Ejercicio1 {
 		// Creamos la clase random
 		Random rand = new Random();
 		
+		// Creamos el scanner
+		Scanner reader = new Scanner(System.in);
+		
 		// Creamos un array bidimensional llamado numerosEnteros de 4 por 5
 		int numerosEnteros[][] = new int[4][5];
 		
@@ -16,7 +21,7 @@ public class Ejercicio1 {
 		boolean busqueda;
 		
 		// Creamos una variable llamada valorBuscar para almacenar el numero que el usuario quiera buscar
-		int valorBuscar;
+		int valorBuscar = 0;
 		
 		// Asignamos los valores del array (numeros enteros aleatorios del 1 al 100)
 		for (int i = 0 ; i < numerosEnteros.length ; i++) {
@@ -25,19 +30,49 @@ public class Ejercicio1 {
 			}
 		}
 		
-		// Mostramos la tabla
+		// Título de la tabla
 		System.out.println("Tabla de números aleatorios del 1 al 100");
 		System.out.println();
 		
+		// Mostramos la tabla
 		for (int i = 0 ; i < numerosEnteros.length ; i++) {
 			for (int j = 0 ; j < numerosEnteros[i].length ; j++) {
 				System.out.print(numerosEnteros[i][j] + "\t");
 			}
 			System.out.println();
 		}
+		System.out.println();
 		
+		// Pedimos al usuario que introduzca el valor a buscar en la tabla
+		do {
+			try {
+				System.out.print("Introduzca un valor a buscar en la tabla: ");
+				valorBuscar = reader.nextInt();
+				assert (valorBuscar >= 1 && valorBuscar <= 100) : "Introduzca un número del 1 al 100 (incluidos)";
+				System.out.println();
+			} catch (AssertionError e) {
+				System.err.println(e.getMessage());
+				System.out.println();
+			} catch (InputMismatchException e) {
+				System.err.println("Introduzca un número entero");
+				System.out.println();
+			} finally {
+				reader.nextLine();
+			}
+		} while (valorBuscar < 1 || valorBuscar > 100);
 		
-		// Preguntamos al usuario que valor quiere buscar
+		// Almacenamos el resultado de la funcion en el booleano busqueda
+		busqueda = buscar(numerosEnteros, valorBuscar);
+		
+		// Mostramos el resultado de la busqueda
+		if (busqueda) {
+			System.out.println("El número SÍ se encuentra en la tabla");
+		} else {
+			System.out.println("El número NO se encuentra en la tabla");
+		}
+		
+		// Cerramos el scanner
+		reader.close();
 		
 	}
 	
@@ -46,7 +81,14 @@ public class Ejercicio1 {
 		// Creamos el booleano a devolver y lo inicializamos a false 
 		boolean busqueda = false;
 		
-		
+		// Recorremos la tabla comprobando si el valor a buscar es igual al numero actual
+		for (int i = 0 ; i < t.length && !busqueda; i++) {
+			for (int j = 0 ; j < t[i].length ; j++) {
+				if (t[i][j] == valor) {
+					busqueda = true;
+				}
+			}
+		}
 		
 		// Devolvemos el booleano
 		return busqueda;
---
+++ Tema3/src/boletin5/Ejercicio2.java
@@ -0,0 +1,29 @@
+package boletin5;
+
+import java.util.Random;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		// Creamos la clase random
+		Random rand = new Random();
+		
+		// Creamos la tabla ordenada de los números del 1 al 10
+		int tablaOrdenada[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+		// Título de la tabla 
+		System.out.println("1 al 10");
+		System.out.println();
+	}
+	
+	static int[] desordenar(int t[]) {
+		
+		int tablaDesordenada[] = t;
+		
+		
+		
+		return tablaDesordenada;
+	}
+
+}
